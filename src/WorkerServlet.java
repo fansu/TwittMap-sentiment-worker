27c65d40a3f894db488378dfeb916aaa68637f42
Fan Su <fs2488@columbia.edu>
2014-12-10 18:00:37 -0500

    remove unused codes

--- src/WorkerServlet.java
+++ src/WorkerServlet.java
@@ -1,45 +1,21 @@
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+
 import java.nio.charset.Charset;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClient;
-import com.amazonaws.services.sqs.model.DeleteMessageRequest;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.kevinsawicki.http.HttpRequest;
 
 /**
@@ -70,7 +46,6 @@ public class WorkerServlet extends HttpServlet {
             throws ServletException, IOException {
 
         try {
-
         	int ch;
             StringBuilder sb = new StringBuilder();
             while((ch = request.getInputStream().read())!= -1)
@@ -78,7 +53,7 @@ public class WorkerServlet extends HttpServlet {
             String m = sb.toString();
             String[] tokens= m.split("::");
 
-			String sentiment = HttpRequest.get("http://access.alchemyapi.com/calls/text/TextGetTextSentiment?apikey=&text=" + tokens[0] + "&outputMode=json").body();
+            String sentiment = HttpRequest.get("http://access.alchemyapi.com/calls/text/TextGetTextSentiment?apikey=&text=" + tokens[0] + "&outputMode=json").body();
 			int start_pos = sentiment.indexOf("type");
 			int end_pos = sentiment.indexOf("score");
 			String stype = sentiment.substring(start_pos+8, end_pos-4);
@@ -88,17 +63,6 @@ public class WorkerServlet extends HttpServlet {
 			        new ClasspathPropertiesFileCredentialsProvider()));
 			sns.setRegion(Region.getRegion(Regions.US_WEST_2));
 			sns.publish("", tokens[5] + "," + tokens[6] + "," + stype);
-/*			JSONObject type_obj = (JSONObject)JSONValue.parse(sentiment);
-			String type =  type_obj.get("type").toString();
-        	final Cluster cluster;
-    		final Session session;
-    		cluster = Cluster.builder().withLoadBalancingPolicy(new DCAwareRoundRobinPolicy("")).addContactPoint("").build();
-    		session = cluster.connect("demo");
-    		
-            String q = "INSERT INTO twitt_sentiment (keywords, id, content, createddate, date, latitude, longitude, sentiment) VALUES (" + workRequest.getMessage().toString() + ", " + type + ") using TTL 5;";
-			session.execute(q);
-
-        */
 
             response.setStatus(200);
 
